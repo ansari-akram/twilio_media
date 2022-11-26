adb9de1e9341882879a8811122c1f69806b24860
ansari-akram <ansariakramobaid@gmail.com>
2022-11-26 15:54:16 +0530

    testing

---
+++ media_twilio_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MediaTwilioAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'media_twilio_app'
---
+++ media_twilio_app/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+@csrf_exempt
+def message(request):
+    user = request.POST.get('From')
+    message = request.POST.get('Body')
+    media_url = request.POST.get('MediaUrl0')
+    print(f'{user} sent {message}')
+
+    response = MessagingResponse()
+    if media_url:
+        r = requests.get(media_url)
+        content_type = r.headers['Content-Type']
+        username = user.split(':')[1]  # remove the whatsapp: prefix from the number
+        if content_type == 'image/jpeg':
+            filename = f'uploads/{username}/{message}.jpg'
+        elif content_type == 'image/png':
+            filename = f'uploads/{username}/{message}.png'
+        elif content_type == 'image/gif':
+            filename = f'uploads/{username}/{message}.gif'
+        else:
+            filename = None
+        if filename:
+            if not os.path.exists(f'uploads/{username}'):
+                os.mkdir(f'uploads/{username}')
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+            response.message('Thank you! Your image was received.')
+        else:
+            response.message('The file that you submitted is not a supported image type.')
+    else:
+        response.message('Please send an image!')
+
+    return HttpResponse('Hello!')
